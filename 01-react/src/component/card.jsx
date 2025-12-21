e9e165bbb2d772c66a5d54979ccd5a5ff3c5f7ca
Rajnish Kumar <rajnishkr.22.11.48@gmail.com>
2025-12-21 23:19:35 +0530

    Update card.jsx

--- 01-react/src/component/card.jsx
+++ 01-react/src/component/card.jsx
@@ -5,7 +5,8 @@ const Card = (props) => {
     <div className = "card">
       <img src={props.img} alt="" />
       <h1>{props.user},{props.age}</h1>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>      <button>View profile</button>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>      
+      <button>View profile</button>
     </div>
   )
 }
